d99af7c09a68596dd86dfb0c27783e5e58cdfabe
developer <team-developer@gmail.com>
2020-06-12 18:31:10 +0800

    fix get eth

---
+++ ci/update_appinfo.js
@@ -0,0 +1,45 @@
+const DateFormat = require('fast-date-format'),
+  fs = require('fs-extra'),
+  path = require('path'),
+  pkgJson = require('../package.json');
+
+const r = (...p) => path.resolve(__dirname, '../', ...p)
+
+const infoPath = 'src/bascore/info.json';
+
+const infoFile = r(infoPath);
+
+const dateFormat = new DateFormat('YYMMDD_HHmm')
+
+updateAppInfo(infoFile)
+
+
+async function updateAppInfo(infoFilePath){
+  fs.ensureFileSync(infoFilePath)
+
+  const ver = pkgJson.version||'2.1.0';
+  const name = pkgJson.name || 'bas-exchange'
+  const author = pkgJson.author || "bas-team"
+  const tagts = dateFormat.format(new Date())
+  const buildTag = `${ver}_${tagts}`;
+
+
+  const info = fs.readJsonSync(infoFilePath) || {}
+
+  const infoJson = Object.assign({},info,{
+    "name":name,
+    "author":author,
+    "version":ver,
+    "buildTag": buildTag,
+    "description":"BAS Chain Exchange"
+  })
+
+  fs.outputJsonSync(
+    infoFilePath,
+    infoJson,
+    {
+      spaces: 2,
+      EOL: '\n'
+    }
+  )
+}
