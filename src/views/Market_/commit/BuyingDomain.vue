89a69e7051cdc3e3f78f710ae83fa5e3a0ecabdb
guoxiaofu <guoxiaofu@coolpi360.com>
2020-06-16 14:46:01 +0800

    a

--- src/views/Market_/commit/BuyingDomain.vue
+++ src/views/Market_/commit/BuyingDomain.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="bas-gray-bg w-100">
     <div class="row justify-content-center align-items-center pt-5 pb-5">
-      <div class="col-5 market-buying-wrap" v-loading.lock="ctrl.loading">
+      <div class="market-buying-wrap" v-loading.lock="ctrl.loading">
         <div class="market-buying--asset">
           <div class="bas-inline-flex">
-            <h5>
+            <p>
               {{$t('p.MarketBuyingDomain')}} <span>{{domaintext}}</span>
-            </h5>
+            </p>
             <div class="domain-type-badge">
               <span >{{$t(`g.${getDomainType}`)}}</span>
             </div>
           </div>
           <div class="bas-inline-flex">
             <div class="bas-info-label">{{$t('l.Owner')}}</div>
-            <div class="bas-info-text">{{asset.owner}}</div>
+            <div class="bas-info-text">aaaa{{asset.owner}}</div>
           </div>
           <div class="bas-inline-flex">
             <div class="bas-info-label">{{$t('l.ExpiredDate')}}</div>
@@ -21,32 +21,32 @@
           </div>
         </div>
 
-        <div v-if="!!topasset.owner" class="market-buying--top mt-4">
+        <div v-if="!topasset.owner" class="market-buying--top">
           <div class="bas-header-tool">
-            <h6>
+            <p>
                {{$t('p.DomainRegistSubRootInfoTitle')}}
-            </h6>
-            <div>
-              <a class="bas-link" @click="gotoTopWhois(topasset.name)">who is>></a>
+            </p>
+            <div class="go-who">
+              <a class="buy-color bas-link market-whois" @click="gotoTopWhois(topasset.name)">who is>></a>
             </div>
           </div>
           <div class="bas-inline-flex">
-            <div class="bas-info-label">{{$t('p.ApplyTabRootName')}}</div>
-            <div class="bas-info-text">{{topasset.name}}</div>
+            <div class="bas-info-label--sub">{{$t('p.ApplyTabRootName')+'：'}}</div>
+            <div class="bas-info-text--sub">aaaa{{topasset.name}}</div>
           </div>
           <div class="bas-inline-flex">
-            <div class="bas-info-label">{{$t('l.Owner')}}</div>
-            <div class="bas-info-text">{{topasset.owner}}</div>
+            <div class="bas-info-label--sub">{{$t('l.Owner')+'：'}}</div>
+            <div class="bas-info-text--sub">{{topasset.owner}}</div>
           </div>
           <div class="bas-inline-flex">
-            <div class="bas-info-label">{{$t('l.ExpiredDate')}}</div>
-            <div class="bas-info-text">{{getTopAssetExpireDate}}</div>
+            <div class="bas-info-label--sub">{{$t('l.ExpiredDate')+'：'}}</div>
+            <div class="bas-info-text--sub">{{getTopAssetExpireDate}}</div>
           </div>
         </div>
-
+        <hr/>
         <div class="market-buying--foot mt-4">
           <div class="bas-inline-flex--center">
-            <span class="payment-prefix">{{$t('l.PaymentAmout')}}</span>
+            <span class="payment-prefix">{{$t('l.PaymentAmout')+'：'}}</span>
             <span class="bas-number">{{pricevol}}</span>
           </div>
           <div class="bas-inline-flex--center">
@@ -195,7 +195,46 @@ export default {
   },
 }
 </script>
+
 <style>
+hr {
+  margin-bottom: 23px;
+}
+.go-who {
+  margin-bottom: 12px;
+}
+.buy-color {
+  color:rgba(0,202,155,1) !important;
+}
+.bas-info-label--sub,
+.bas-info-text--sub {
+  height:22px;
+  font-size:16px;
+  font-family:PingFangSC-Light,PingFang SC;
+  font-weight:300;
+  color:rgba(4,6,46,1);
+  line-height:22px;
+  display: flex;
+  margin-bottom: 5px;
+}
+.bas-info-text,
+.bas-info-label {
+  font-family:PingFangSC-Regular,PingFang SC;
+  font-weight:400;
+}
+.bas-info-text {
+  height:22px;
+  font-size:16px;
+  color:rgba(4,6,46,1);
+  line-height:22px;
+}
+.bas-info-label {
+  height:25px;
+  font-size:18px;
+  color:rgba(150,150,166,1);
+  line-height:25px;
+  margin-bottom: 8px;
+}
 .market-buying--foot {
   display: inline;
   text-align: center;
@@ -208,10 +247,20 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.bas-inline-flex--center > button {
+  height: 52px;
+  font-size:20px;
+  font-family:PingFangSC-Semibold,PingFang SC;
+  font-weight:600;
+  color:rgba(255,255,255,1);
+  line-height:28px;
+  letter-spacing:1px;
+}
 
 .bas-inline-flex--center>span.bas-number {
   font-size: 36px;
   color:  #00CA9B ;
+  font-weight: Semibold;
 }
 
 .bas-inline-flex--center>span.bas-number::after {
@@ -221,24 +270,31 @@ export default {
   margin-left: .25rem;
 }
 .bas-inline-flex--center>span.payment-prefix {
-  color:  #00CA9B ;
-  font-size: 1rem;
+  /* color:  #00CA9B ; */
+  /* font-size: 1rem; */
   margin-right: .25rem;
+  font-size:18px;
+  font-family:PingFangSC-Regular,PingFang SC;
+  font-weight:400;
+  color:rgba(0,202,155,1);
+  line-height:25px;
 }
 
 .market-buying-wrap {
+  width: 50%;
   background: rgba(255,255,255,.8);
   padding: 1.5rem 1rem;
 }
 .market-buying--asset * ,.market-buying--top * {
-  line-height: 1.2rem;
-  margin-bottom: auto;
-  justify-self: start;
+  /* line-height: 1.2rem; */
+  /* margin-bottom: auto; */
+  /* justify-self: start; */
 }
 
 .domain-type-badge {
   margin-left: 1.25rem;
-  margin-bottom: .5rem;
+  margin-top: 4px;
+  margin-bottom: auto;
   line-height: 1.2rem;
   color:rgba(16,205,161,1);
   background:rgba(16,205,161,.1);
@@ -250,11 +306,28 @@ export default {
   font-size: 14px;
   margin: auto .75rem;
 }
-
-.market-buying--asset {
-  padding: 1.25rem;
+.bas-inline-flex > p {
+  height:30px;
+  font-size:22px;
+  font-family:PingFangSC-Regular,PingFang SC;
+  font-weight:400;
+  color:rgba(4,6,46,1);
+  line-height:30px;
+  letter-spacing:1px;
+  margin-bottom: 22px;
 }
-
+.market-buying--asset {
+  padding: 20px 120px 20px 120px;
+}
+.bas-header-tool > p {
+  height:25px;
+  font-size:18px;
+  font-family:PingFangSC-Regular,PingFang SC;
+  font-weight:400;
+  color:rgba(4,6,46,1);
+  line-height:25px;
+  margin-bottom: 7px;
+}
 .bas-header-tool {
   width: 100%;
   display: inline-flex;
@@ -263,11 +336,13 @@ export default {
 }
 
 .market-buying--top {
-  padding: 1.25rem;
+  padding: 20px;
+  margin: 20px 100px 20px 100px;
   border-radius:1px;
   background:rgba(245,246,246,1);
   opacity:0.75;
   border:1px solid rgba(235,237,237,1);
+  /* margin-bottom: 24px; */
 }
 </style>
 
