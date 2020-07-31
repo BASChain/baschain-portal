bef68a2696fb2be6b88c78208f8fc269390a0f4d
dev-fronter <team-developer@gmail.com>
2020-07-31 11:16:46 +0800

    update version

--- src/bizlib/apps/index.js
+++ src/bizlib/apps/index.js
@@ -3,7 +3,7 @@
  * bcoibbecpnakojfbbpilgmmmlgnfklea 1.0.3
  */
 export const ChromeExtensionStore =
-         "https://chrome.google.com/webstore/detail/bas/bcoibbecpnakojfbbpilgmmmlgnfklea?hl=zh-CN&authuser=1";
+         "https://chrome.google.com/webstore/detail/bas/bcoibbecpnakojfbbpilgmmmlgnfklea";
 export const FirefoxExtensionStore = 'https://addons.mozilla.org/zh-CN/firefox/addon/bas/?src=search'
 export const RelaDownloadBasePath = '/apps'
 export const ExtFansBase ="https://www.extfans.com/search/extensions"
